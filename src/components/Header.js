c4dbe6e491095f54f3541cff63f308f87943542b
Ben Greanya <bengreanya@gmail.com>
2023-01-31 15:41:51 -0800

    user displaying in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useUser } from '../context/UserContext.js';
+import { signOut } from '../services/auth.js';
 
 export default function Header() {
-  return <div>Header</div>;
+  const { user, setUser } = useUser();
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      setUser(null);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+  return (
+    <>
+      {user && (
+        <nav>
+          <p>Hello {user.email}</p>
+          <button onClick={handleLogout}>Sign Out</button>
+        </nav>
+      )}
+    </>
+  );
 }
