3d93b238f4d90abf538e764ce04739de8be54f40
Ben Greanya <bengreanya@gmail.com>
2023-01-31 17:50:19 -0800

    added createtask to task.js

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -4,3 +4,7 @@ export async function getTasks() {
   const response = await client.from('to-do').select();
   return checkError(response);
 }
+export async function createTask(description) {
+  const response = await client.from('to-do').insert({ description }).single();
+  return checkError(response);
+}
