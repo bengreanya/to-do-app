2393049ac4f00198d7be067a2baf347d595f06a6
Ben Greanya <bengreanya@gmail.com>
2023-02-02 10:45:03 -0800

    taskform changes

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { TasksContext } from '../context/TasksContext.js';
 
 export default function TaskForm() {
+  const { setTasks } = useContext(TasksContext);
+  const handleNewTask = async () => {};
+
   return <div>TaskForm</div>;
 }
